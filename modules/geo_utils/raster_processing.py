927225d8333d399c935135c5b4922f46cb17cd62
Pranavesh Panakkal <71752378+Pranavesh-Panakkal@users.noreply.github.com>
2022-04-08 15:50:47 -0500

    Update raster_processing.py

--- modules/geo_utils/raster_processing.py
+++ modules/geo_utils/raster_processing.py
@@ -22,11 +22,6 @@ def subtract_two_raster(
     :rtype: None
     """
     _main_raster = rxr.open_rasterio(raster_to_subtract_from, masked=True).squeeze()
-    # _main_raster = rxr.open_rasterio(raster_to_subtract_from).squeeze()
     _second_raster = rxr.open_rasterio(subtracting_raster, masked=True).squeeze()
-    # _second_raster_match = _second_raster.rio.reproject_match(_main_raster)
-    # _second_raster_match = _second_raster_match.assign_coords({"x": _main_raster.x, "y": _main_raster.y})
-    # _second_raster = rxr.open_rasterio(subtracting_raster).squeeze()
-    # _difference = _main_raster - _second_raster_match
     _difference = _main_raster - _second_raster
     _difference.rio.to_raster(file_path_to_save)
